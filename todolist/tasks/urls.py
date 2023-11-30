13f2946dc4e0ef3b01cdaf7142f460cc96aea48e
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2023-11-30 21:16:51 +0530

    Activate admin page

--- todolist/tasks/urls.py
+++ todolist/tasks/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import dashboard, create_task, update_task, delete_task, user_profile, register, user_login, user_logout
 
 urlpatterns = [
+    path('', dashboard, name='dashboard'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('', dashboard, name='dashboard'),
     path('create/', create_task, name='create_task'),
     path('update/<int:task_id>/', update_task, name='update_task'),
     path('delete/<int:task_id>/', delete_task, name='delete_task'),
